6c4152731516b2f7119ab3d6dce5eb1939cdb90f
James Pacheco <jamesrp13@gmail.com>
2022-04-20 19:07:41 -0600

    Remove unnecessary elements

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,19 +1,19 @@
 
 import { FC, ReactNode } from "react";
-import * as web3 from '@solana/web3.js'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-require('@solana/wallet-adapter-react-ui/styles.css');
+import * as web3 from '@solana/web3.js'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const wallet = new PhantomWalletAdapter()
+    const wallets = [new PhantomWalletAdapter()]
 
     const endpoint = web3.clusterApiUrl('devnet')
 
     return (
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={[wallet]}>
+            <WalletProvider wallets={wallets}>
                 <WalletModalProvider>
                     { children }
                 </WalletModalProvider>
