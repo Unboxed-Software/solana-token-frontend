aebd1da2a546722ced2605bd6c8e95143bb7644d
john <johnzhengyuliu@gmail.com>
2022-05-09 18:06:56 -0500

    create mint

---
+++ components/CreateMint.tsx
@@ -0,0 +1,77 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
+import { FC, useState } from "react";
+import styles from "../styles/Home.module.css";
+import {
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  getMinimumBalanceForRentExemptMint,
+  createInitializeMintInstruction,
+} from "@solana/spl-token";
+
+import * as nacl from "tweetnacl";
+import * as bs58 from "bs58";
+
+export const CreateMintForm: FC = () => {
+  const [txSig, setTxSig] = useState("");
+  const { connection } = useConnection();
+  const { wallet, publicKey, sendTransaction } = useWallet();
+  const link = () => {
+    return txSig
+      ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet`
+      : "";
+  };
+
+  const createMint = async (event) => {
+    event.preventDefault();
+    if (!connection || !publicKey) {
+      return;
+    }
+    const transaction = new web3.Transaction();
+    // const recipientPubKey = new web3.PublicKey(event.target.recipient.value);
+
+    const mint = web3.Keypair.generate();
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+    transaction.add(
+      web3.SystemProgram.createAccount({
+        fromPubkey: publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMintInstruction(
+        mint.publicKey,
+        2,
+        publicKey,
+        publicKey,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    sendTransaction(transaction, connection).then((sig) => {
+      setTxSig(sig);
+    });
+  };
+
+  return (
+    <div>
+      {publicKey ? (
+        <form onSubmit={createMint} className={styles.form}>
+          <button type="submit" className={styles.formButton}>
+            Send
+          </button>
+        </form>
+      ) : (
+        <span>Connect Your Wallet</span>
+      )}
+      {txSig ? (
+        <div>
+          <p>View your transaction on </p>
+          <a href={link()}>Solana Explorer</a>
+        </div>
+      ) : null}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,29 @@
-import { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import WalletContextProvider from '../components/WalletContextProvider'
-import { AppBar } from '../components/AppBar'
-import { BalanceDisplay } from '../components/BalanceDisplay'
-import { SendSolForm } from '../components/SendSolForm'
-import Head from 'next/head'
+import { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import WalletContextProvider from "../components/WalletContextProvider";
+import { AppBar } from "../components/AppBar";
+import { BalanceDisplay } from "../components/BalanceDisplay";
+import { SendSolForm } from "../components/SendSolForm";
+import { CreateMintForm } from "../components/CreateMint";
+import Head from "next/head";
 
 const Home: NextPage = (props) => {
-
   return (
     <div className={styles.App}>
       <Head>
-        <title>Wallet-Adapter Example</title>
-        <meta
-          name="description"
-          content="Wallet-Adapter Example"
-        />
+        <title>Token Program</title>
+        <meta name="description" content="Token Program" />
       </Head>
       <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
           <BalanceDisplay />
-          <SendSolForm />
+          {/* <SendSolForm /> */}
+          <CreateMintForm />
         </div>
-      </WalletContextProvider >
+      </WalletContextProvider>
     </div>
   );
-}
+};
 
 export default Home;
