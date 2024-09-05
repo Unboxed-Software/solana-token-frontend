efb7b7da88604d7f07e65ccdf15a5e92a64a8c43
Tarun <tarunsanjayrao@gmail.com>
2024-09-05 14:01:48 +0530

    solana-dapp

--- README.md
+++ README.md
@@ -1,36 +1,50 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# solana
+
+This project is generated with the [create-solana-dapp](https://github.com/solana-developers/create-solana-dapp) generator.
 
 ## Getting Started
 
-First, run the development server:
+### Prerequisites
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+- Node v18.18.0 or higher
+
+### Installation
+
+#### Clone the repo
+
+```shell
+git clone <repo-url>
+cd <repo-name>
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+#### Install Dependencies
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+```shell
+npm install
+```
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+#### Start the web app
 
-## Learn More
+```
+npm run dev
+```
 
-To learn more about Next.js, take a look at the following resources:
+## Apps
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### web
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+This is a React app.
 
-## Deploy on Vercel
+#### Commands
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+Start the web app
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+```shell
+npm run dev
+```
+
+Build the web app
+
+```shell
+npm run build
+```
---
+++ web/components/tokenaccount/MintToForm.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import * as web3 from '@solana/web3.js';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { FC, useState } from 'react';
+import styles from '../../app/styles/Home.module.css';
+import {
+  createMintToInstruction,
+  getAssociatedTokenAddress,
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getAccount,
+} from '@solana/spl-token';
+
+export const MintToForm: FC = () => {
+  const [txSig, setTxSig] = useState('');
+  const [tokenAccount, setTokenAccount] = useState('');
+  const [balance, setBalance] = useState('');
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+  const link = () => {
+    return txSig
+      ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet`
+      : '';
+  };
+
+  const mintTo = async (event: any) => {
+    event.preventDefault();
+    if (!connection || !publicKey) {
+      return;
+    }
+
+    // BUILD AND SEND MINT TRANSACTION HERE
+  };
+
+  return (
+    <div>
+      <br />
+      {publicKey ? (
+        <form onSubmit={mintTo} className={styles.form}>
+          <label htmlFor="mint">Token Mint:</label>
+          <input
+            id="mint"
+            type="text"
+            className={styles.formField}
+            placeholder="Enter Token Mint"
+            required
+          />
+          <label htmlFor="recipient">Recipient:</label>
+          <input
+            id="recipient"
+            type="text"
+            className={styles.formField}
+            placeholder="Enter Recipient PublicKey"
+            required
+          />
+          <label htmlFor="amount">Amount Tokens to Mint:</label>
+          <input
+            id="amount"
+            type="text"
+            className={styles.formField}
+            placeholder="e.g. 100"
+            required
+          />
+          <button type="submit" className={styles.formButton}>
+            Mint Tokens
+          </button>
+        </form>
+      ) : (
+        <span></span>
+      )}
+      {txSig ? (
+        <div>
+          <p>Token Balance: {balance} </p>
+          <p>View your transaction on </p>
+          <a href={link()}>Solana Explorer</a>
+        </div>
+      ) : null}
+    </div>
+  );
+};
